60a9289b6b20204bb16d7764290a40fadeaadaa4
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-08-23 16:19:44 +0200

    Renamed app to examples

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,16 @@ gulp.task('default', ['watch']);
 
 // configure the jshint task
 gulp.task('jshint', function() {
-    return gulp.src('app/**/*.js')
+    return gulp.src('lib/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-    gulp.watch('app/**/*.js', ['jshint']);
+    gulp.watch('lib/**/*.js', ['jshint']);
 
     // Start up the server and have it reload when anything in the
     // ./build/ directory changes
-    nodemon({script: 'app/server.js', watch: 'app/**'});
+    nodemon({script: 'examples/server.js', watch: 'examples/**'});
 });
