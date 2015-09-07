b5b767e52d617af5f706f3e5378441facc8a1984
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-09-08 00:16:24 +0200

    Add registration.js service as a separate module

--- examples/server.js
+++ examples/server.js
@@ -18,8 +18,46 @@ var req = coap.request('coap://localhost/')
 req.on('response', function(res) {
     res.pipe(process.stdout);
     res.on('end', function() {
-        process.exit(0)
+        //process.exit(0)
     });
 });
 
 req.end();
+
+/** Register */
+coap
+    .request(
+        {
+            host: 'localhost',
+            method: 'POST',
+            query: 'HELLO REGISTER',
+            pathname: '/rd'
+        })
+    .end();
+
+/** Update */
+coap
+    .request(
+        {
+            host: 'localhost',
+            method: 'PUT',
+            query: 'HELLO UPDATE',
+            pathname: '/rd'
+        })
+    .end();
+
+/** Deregister */
+coap
+    .request(
+        {
+            host: 'localhost',
+            method: 'DELETE',
+            query: 'HELLO DEREGISTER',
+            pathname: '/rd'
+        })
+
+    .on('end', function() {
+        process.exit(0);
+    })
+    
+    .end();
